622a94cf4bb8fac3a9b56d53f495dc82948d13d9
subham-kumar-au17 <subham4663@gmail.comq>
2022-02-19 17:42:54 +0530

    fun-over

---
+++ node/js-in-nutshell/play.js
@@ -0,0 +1,59 @@
+/*
+var name = "Shubham";
+var age = 24;
+var hasHobbies = true;
+
+function summarizerUser(userName, userAge, userHobbies) {
+    return (
+        "Name is " +
+        userName +
+        " age is " +
+        userAge +
+        " his hobbies are: " +
+        hasHobbies
+    );
+}
+
+console.log(summarizerUser(name, age, hasHobbies));
+
+// Arrow Function 
+
+const addTwo = (a) => a+1;
+
+console.log(addTwo(5));
+
+let x = 3;
+let y = 4;
+
+const checkArrow = () => x + y;
+
+console.log(checkArrow())
+
+*/
+
+// Object, Properties & Methods 
+
+
+const person = {
+    name: 'Kumar',
+    age: 27,
+    // method
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const hobbies = ['a', 'b', 'c', 'd'];
+
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+console.log(hobbies);
+console.log(hobbies.map());
+
+
+
+
+
+
+
